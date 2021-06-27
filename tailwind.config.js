0f76f84d22a10885230fb0640ee8aa171cd512b9
Kilari Teja <kilariteja9@gmail.com>
2021-06-27 15:56:21 +0530

    Add safelist to tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    safelist: [
+      'text-blue-600',
+    ],
+    enabled: true,
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
