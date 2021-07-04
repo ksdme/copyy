e0f4126271f084c95395a9d6fd75a6350677fdae
Kilari Teja <kilariteja9@gmail.com>
2021-07-04 14:58:07 +0530

    Use simpler and smaller code

--- hooks/useCode.ts
+++ hooks/useCode.ts
@@ -1,4 +1,5 @@
-import random from 'random-words'
+import random from 'random'
+import words from '../data/words'
 
 /*
   Return code for app from the url code.
@@ -20,12 +21,17 @@ export function toUrlCode(code: string) {
   Generates a new code and returns it.
 */
 export function generateCode() {
-  // Generate the random words.
-  const words = random({
-    min: 3,
-    max: 5,
-  })
+  // The number of words in the code.
+  const count = random.integer(3, 5)
+
+  // Words in the code.
+  const code = []
+
+  // Pick a word from the wordlist at random.
+  for (let index = 0; index < count; index++) {
+    code.push(words[random.integer(0, words.length - 1)])
+  }
 
   // Combine the words.
-  return toUrlCode(words.join('-'))
+  return toUrlCode(code.join('-'))
 }
